ecffa9a6e8b15cefebab8b2b98fc25c1bc305977
Aleksandra <aleksandra.bogusz@kainos.com>
2021-07-06 09:59:35 +0200

    setting navigation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,29 @@
+import NavBar from "./NavBar";
+import Capability from "./Capability/Capability";
+import Role from "./Role/Role";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const App = () => {
-    return <div>Hello from App!</div>
+  return (
+    <div>
+      <Router>
+        <Switch >
+          <Route exact path="/">
+            <NavBar />
+          </Route>
+          <Route exact path="/capability">
+            <NavBar />
+            <Capability />
+          </Route>
+          <Route exact path="/role">
+            <NavBar />
+            <Role/>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

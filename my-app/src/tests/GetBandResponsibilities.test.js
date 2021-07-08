b4a477dbb82546de3a409765029fd6d7373f34d9
Toby Bell <toby.bell@kainos.com>
2021-07-08 16:50:35 +0100

    fix

--- my-app/src/tests/GetBandResponsibilities.test.js
+++ my-app/src/tests/GetBandResponsibilities.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import GetBandResponsibilities from "./Band/GetBandResponsibilities";
+import GetBandResponsibilities from "../Band/GetBandResponsibilities";
 
 it('Band responsibility View renders without crashing', () => {
   const div = document.createElement('div');

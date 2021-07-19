c2e6d216d4a3f701badced9b100466a0829c2e51
Joe Downard <joseph.downard@kainos.com>
2021-07-19 15:06:54 +0100

    Added authentication to band pages

--- my-app/src/Band/GetBandResponsibilities.js
+++ my-app/src/Band/GetBandResponsibilities.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 import axios from "axios";
 import './Band.css'
 import ErrorPage from "../shared/ErrorPage";
+import { useAuth0 } from '@auth0/auth0-react';
 
 import { Table } from "react-bootstrap";
 
@@ -10,59 +11,92 @@ const GetBandResponsibilities = () => {
     const [searchTerm, setSearchTerm] = useState("");
     const [results, setResults] = useState();
     const [list, setList] = useState();
+    const { getAccessTokenSilently, user } = useAuth0();
+    const [error, setError] = useState();
 
-
- useEffect(() => {
+    useEffect(() => {
         if (!results) {
-            async function fetchResults() {
-                const res = await axios.get(`https://my.api:50001/getBandResponsibilities`);
-                console.log(res);
-                setResults(res.data);
+            const fetchResults = async () => {
+                let token;
+                try {
+                    const options = {
+                        audience: 'http://my.api:50001',
+                        scope: 'read:secured'
+                    }
+                    token = await getAccessTokenSilently(options);
+                    console.log(token)
+                } catch (e) {
+                    console.log(e)
+                }
+
+
+                try {
+                    const config = {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        }
+                    }
+                    const res = await axios.get(`https://my.api:50001/getBandResponsibilities`, config);
+                    setResults(res.data);
+
+                } catch (error) {
+                    if (error.response.status === 403 || error.response.status === 401) {
+                        setError(error.response.status);
+                    }
+
+                }
             }
             fetchResults();
         } else {
             let tempList = results.filter((r) => {
-                const {BandName} = r
-                return ( BandName.includes(searchTerm) ||  searchTerm === "");
+                const { BandName } = r
+                return (BandName.includes(searchTerm) || searchTerm === "");
             }).map((r) => {
-                const {BandName, BandLevel, Responsibilities} = r
-            return (
-                <tr >
-                    <td>{BandName}</td>
-                    <td>{BandLevel}</td>
-                    <td>{Responsibilities}</td>
-                </tr>
-            )
+                const { BandName, BandLevel, Responsibilities } = r
+                return (
+                    <tr >
+                        <td>{BandName}</td>
+                        <td>{BandLevel}</td>
+                        <td>{Responsibilities}</td>
+                    </tr>
+                )
 
             })
             setList(tempList);
         }
     }, [results, searchTerm]);
 
-    return (
-        <div>
-            <FormLabel
+    if (error) {
+        console.log(error)
+        return (<ErrorPage error={error} />)
+    } else if (results) {
+        return (
+            <div>
+                <FormLabel
                     label="Search Term"
                     className="searchBar"
                 >
-                    <Form.Control type="search" placeholder="Search for a Band Name" onChange={(e) => setSearchTerm(e.target.value)}/>
+                    <Form.Control type="search" placeholder="Search for a Band Name" onChange={(e) => setSearchTerm(e.target.value)} />
                 </FormLabel>
-            <div className="emp-table">
-                <Table >
-                    <thead>
-                        <tr>
-                            <th>Band Name</th>
-                            <th>Band Level</th>
-                            <th>Responsibilities</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {list}
-                    </tbody>
-                </Table>
+                <div className="emp-table">
+                    <Table >
+                        <thead>
+                            <tr>
+                                <th>Band Name</th>
+                                <th>Band Level</th>
+                                <th>Responsibilities</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {list}
+                        </tbody>
+                    </Table>
+                </div>
             </div>
-            </div>
-    )
+        )
+    } else {
+        return <div> Loading data!</div>
+    }
 }
 
 export default GetBandResponsibilities;

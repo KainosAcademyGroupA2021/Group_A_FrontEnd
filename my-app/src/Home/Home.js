b37fd4efffbd88ecc2738d6ab4f6a82826fd2136
Jordan <jordan.stevenson@kainos.com>
2021-07-20 09:04:55 +0100

    Home navigation and some minor changes

--- my-app/src/Home/Home.js
+++ my-app/src/Home/Home.js
@@ -8,6 +8,8 @@ import {
   Row,
   Col,
   Image,
+  ListGroup,
+  ListGroupItem
 } from "react-bootstrap";
 import "./Home.css";
 import capabilityImg from "../Images/capability.png";
@@ -18,47 +20,50 @@ import jobImg from "../Images/job.png";
 const Home = () => {
   return (
     
-    <Container fluid style={{ width: "100%", allignItems:"center"}}>
+    <Container fluid style={{ width: "100%", justifyContent:"center", allignItems:"center", marginTop: "100px", display: "flex" }}>
 
       {/* <Container fluid style={{marginTop: "10px", marginLeft: "150px",width: "100%"}}> */}
         <Row>
-          <Col style={{marginTop: "10px"}}>
-            <Card style={{ width: "18rem" }}>
+          <Col style={{ maxWidth: "400px"}}>
+            <Card style={{ width: "20rem"}}>
               <Card.Img as={Image} src={capabilityImg} fluid={true} alt="Card image" />
               <Card.Body>
-               
-                <Button variant="outline-primary" size="lg" style={{width: "15rem"}}>Capability</Button>
-             
+              <Card.Title>Capability</Card.Title>
+              <ListGroupItem><Button href="/Capability/CapabilityPerJobFamily" variant="outline-primary" size="lg" style={{width: "15rem"}}>Capability Relation with Job Family</Button></ListGroupItem>
+              <ListGroupItem><Button href="/Capability/AddJobFamily" variant="outline-primary" size="lg" style={{width: "15rem"}}>View Capability Leads</Button></ListGroupItem>
+              <ListGroupItem><Button href="/Capability/AddCapability" variant="outline-primary" size="lg" style={{width: "15rem"}}>Add Job Family</Button></ListGroupItem>
+              <ListGroupItem><Button href="/Capability/GetCapability" variant="outline-primary" size="lg" style={{width: "15rem"}}>Add Capability</Button></ListGroupItem>
+              <ListGroupItem><Button href="/Capability/CapabilityLead" variant="outline-primary" size="lg" style={{width: "15rem"}}>Edit Capability</Button></ListGroupItem>    
               </Card.Body>
             </Card>
           </Col>
 
-          <Col style={{marginTop: "10px"}}>
-            <Card style={{ width: "18rem" }}>
+          <Col style={{maxWidth: "400px"}}>
+            <Card style={{ width: "20rem" }}>
             <Card.Img as={Image} src={bandImg} fluid={true} alt="Card image" />
-              <Card.Body>
-                <Button variant="outline-primary" size="lg" style={{width: "15rem"}}>Band</Button>
+              <Card.Body>   
+              <Card.Title>Roles</Card.Title>
+              <ListGroupItem><Button href="/role/GetJobRoles" variant="outline-primary" size="lg" style={{width: "15rem"}}>Job View Roles</Button></ListGroupItem>
+              <ListGroupItem><Button href="/role/addRole" variant="outline-primary" size="lg" style={{width: "15rem"}}>Add a Role</Button></ListGroupItem>
+              <ListGroupItem><Button href="/role/adminRoleView" variant="outline-primary" size="lg" style={{width: "15rem"}}>Admin Role View</Button></ListGroupItem>
               </Card.Body>
             </Card>
           </Col>
 
-          <Col style={{marginTop: "10px"}}>
-            <Card style={{ width: "18rem" }}>
+          <Col style={{maxWidth: "400px"}}>
+            <Card style={{ width: "20rem" }}>
             <Card.Img as={Image} src={roleImg} fluid={true} alt="Card image" />
-              <Card.Body>
-                <Button variant="outline-primary" size="lg" style={{width: "15rem"}}>Role</Button>
+            <Card.Body>   
+              <Card.Title>Band</Card.Title>
+              <ListGroupItem><Button href="/Band/GetBandResponsibilities" variant="outline-primary" size="lg" style={{width: "15rem"}}>View Band Responsibilties</Button></ListGroupItem>
+              <ListGroupItem><Button href="/band/getTrainingBand" variant="outline-primary" size="lg" style={{width: "15rem"}}>View training</Button></ListGroupItem>
+              <ListGroupItem><Button href="/band/GetBandCompetencies" variant="outline-primary" size="lg" style={{width: "15rem"}}>Band Competencies</Button></ListGroupItem>
+              <ListGroupItem><Button href="/band/addBand" variant="outline-primary" size="lg" style={{width: "15rem"}}>Add a Band</Button></ListGroupItem>
+              <ListGroupItem><Button href="/band/adminBandView" variant="outline-primary" size="lg" style={{width: "15rem"}}>Admin Band View</Button></ListGroupItem>
               </Card.Body>
             </Card>
           </Col>
 
-          <Col style={{marginTop: "10px"}}>
-            <Card style={{ width: "18rem" }}>
-            <Card.Img as={Image} src={jobImg} fluid={true} alt="Card image" />
-              <Card.Body>
-                <Button variant="outline-primary" size="lg" style={{width: "15rem"}}>Job</Button>
-              </Card.Body>
-            </Card>
-          </Col>
 
         </Row>
 
